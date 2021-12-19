4e524038e79f0f5308c4fe0c411097e2daa7b6f3
devmateusramos <devmateusramos@gmail.com>
2021-12-19 12:46:17 -0300

    component header image arrumado

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./styles.module.scss";
+import Image from "next/image";
+import logoSvg from "../../../public/images/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news' />
+        {/* <img src='/images/logo.svg' alt='ig.news' /> */}
+        <Image src={logoSvg} alt=' ignews seu blog de tecnologia' />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
